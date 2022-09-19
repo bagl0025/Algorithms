657b516c9ceeb78802a78b9f6c853c9eee4f3a93
bagl0025 <bagl0025@umn.edu>
2022-09-19 13:34:37 -0500

    hash table with bucket chains

---
+++ bucket_chaining.js
@@ -0,0 +1,44 @@
+class HashEntry {
+  constructor(key, data) {
+    this.key = key;
+    // data to be stored
+    this.value = data;
+    // reference to new entry
+    this.next = null;
+  }
+}
+
+let entry = new HashEntry(3, "Educative");
+console.log("bcb", String(entry.key) + ", " + entry.value);
+
+class HashTable {
+  //Constructor
+  constructor() {
+    //Size of the HashTable
+    this.slots = 10;
+    //Current entries in the table
+    //Used while resizing the table when half of the table gets filled
+    this.size = 0;
+    //Array of HashEntry objects (by deafult all None)
+    this.bucket = [];
+    for (var i = 0; i < this.slots; i++) {
+      this.bucket[i] = null;
+    }
+  }
+  //Helper Functions
+  get_size() {
+    return this.size;
+  }
+  isEmpty() {
+    return this.get_size() == 0;
+  }
+}
+
+let ht = new HashTable();
+console.log(ht.isEmpty());
+
+//Hash Function
+// getIndex(key) {
+// 	let index = key % this.slots;
+// 	return index;
+// }
