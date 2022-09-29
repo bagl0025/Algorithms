f09c947ea83f9c7f09d5acfd7baa0e4aadca010c
bagl0025 <bagl0025@umn.edu>
2022-09-29 15:46:54 -0500

    update

--- possibleSums.js
+++ possibleSums.js
@@ -1,13 +1,16 @@
 var coins = [10, 50, 100];
 var quantity = [1, 2, 1];
-var arr = [];
-for (i = 0; i < coins.length; i++) {
-  //creates new array of coins
-  arr.push(Array(quantity[i]).fill(coins[i]));
-}
-arr = arr.flat();
+// var coins = [1, 2];
+// var quantity = [50000, 2];
+var arr = [5, 10, 15];
+// for (i = 0; i < coins.length; i++) {
+//   //creates new array of coins
+//   arr.push(Array(quantity[i]).fill(coins[i]));
+// }
+// arr = arr.flat();
 // console.log("arr", arr);
-const combinations = (arr, num) => {
+const combinations = (arr) => {
+  // const combinations = (arr,num) => {
   // num is number of elements to sum
   const res = [];
   let temp,
@@ -18,28 +21,54 @@ const combinations = (arr, num) => {
   for (i = 0; i < max; i++) {
     temp = [];
     for (j = 0; j < arr.length; j++) {
+      // console.log(temp, "temp1");
+
+      ///////////////////////////////////////////////
+      //need to understand ans fix this if state ment//
+      /// running too many timez
+      ///////////////////////
+
       if (i & (1 << j)) {
-        /// ????????????
         //bit shitfing to check for non zero??
         temp.push(arr[j]);
+        console.log(temp, "temp", i, "i");
+        res.push(
+          temp.reduce(function (a, b) {
+            return a + b;
+          })
+        );
       }
     }
-    if (temp.length === num) {
-      res.push(
-        temp.reduce(function (a, b) {
-          return a + b;
-        })
-      );
-    }
+    // if (temp.length === num) {
+    //   res.push(
+    //     temp.reduce(function (a, b) {
+    //       return a + b;
+    //     })
+    //   );
+    //   console.log(temp, "if len=num");
+    // }
   }
   return res;
 };
 var result = [];
-for (i = 1; i < arr.length + 1; i++) {
-  result.push(combinations(arr, i));
-}
+// for (i = 1; i < arr.length + 1; i++) {
+result.push(combinations(arr));
+// }
 result = new Set(result.flat());
-// console.log(result, result.size);
-return x.size;
+console.log(result, result.size);
 
-// drop repeated function call
+// private void arraySumPermutation(int value ,int[] arr){
+//   int N = arr.length;
+//   for(int i=0;i<1<<N;i++){
+//       int sum = 0;
+//       for(int j=0;j<N;j++){
+
+//           if((i & 1<<j)>0){
+//               iCount++;
+//               sum += arr[j];
+//               //S.O.P(sum);
+//           }
+//       }
+
+//   }
+// }
